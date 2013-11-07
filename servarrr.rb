5117f2fd7fd30aea02ef603c11c828c64cacaf27
Brook Riggio <brooke.riggio@gmail.com>
2013-11-06 16:27:08 -0800

    Refactor: Classify, Methodify, DRY up.
    Add a little structure to code with a few methods.

--- servarrr.rb
+++ servarrr.rb
@@ -1,27 +1,47 @@
 require 'socket'
 
-server = TCPServer.new 1337
-puts "Preparrr the cannon on port 1337"
+class Servarrr
+  attr_accessor :client, :server
 
-loop do
-  client = server.accept
-  request = client.gets
-  puts request
-  begin
-    path = request.split(" ")[1]
-    path = File.join("public", path)
-    File.open(path, "r") do |file|
-      puts "found the file: #{path}"
-      client.puts "HTTP/1.1 200 OK"
-      client.puts "Content-Type: text/html"
-      client.puts "Content-Length: #{file.size}"
-      client.puts "Connection: close"
-      client.puts
-      client.puts file.read
+  def run
+    @server = start_server
+
+    loop do
+      @client = @server.accept
+
+      request = @client.gets
+      puts request
+
+      serve_file File.join("public", request.split(" ")[1])
+
+      @client.close
     end
-  rescue Exception => e
-    puts "there was an error: #{e.inspect}"
   end
 
-  client.close
+  def start_server
+    port   = ARGV[0] || 1337
+    puts "Preparrr the cannon on port #{port}"
+    TCPServer.new port
+  end
+
+  def serve_file(path)
+    File.open(path, "r") do |file|
+      response 200, "Content-Type: text/html", file.read
+    end
+  rescue Errno::ENOENT => err
+    response 404, "Content-Type: text/plain", "404: File not found."
+  end
+
+  def response(status, headers, content)
+    @client.puts "HTTP/1.1 #{status}"
+    @client.puts headers
+    @client.puts "Content-Length: #{content.length}"
+    @client.puts "Connection: close"
+    @client.puts
+    @client.puts content
+
+    puts "Responding with #{status}"
+  end
 end
+
+Servarrr.new.run
